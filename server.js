6a1576df0d62f5cd3594d3502540af2c1b5190c4
Richard Kim <rk.sungjin@gmail.com>
2018-07-17 15:08:22 -0700

    path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+// var path = require("path");
 
 var app = express();
 var PORT = process.env.port || 3000;
